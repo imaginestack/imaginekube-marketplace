355095ccfa198e7c0f4081201599d9c12ffa8eb8
cto <cto@imaginekube.com>
2023-06-05 06:40:44 -0700

    Create install.sh

---
+++ argocd/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+kubectl create namespace argocd
+
+helm install argo-cd argo/argo-cd --version 5.31.1 --namespace argocd
