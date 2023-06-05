c7a2a62cda3bad691f35ab9de2562a5696de03c7
cto <cto@imaginekube.com>
2023-06-05 07:08:09 -0700

    Create install.sh

---
+++ ketch/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Sleep for 5 Seconds incase Imagine Cert Manager Install has not scaled to one replica. Ketch v0.6.1 handles this better.
+sleep 5
+
+#Incase Sleep does not work, can leverage a kubectl wait for the Cert Manager deployment to be done.
+kubectl wait --for=condition=available --timeout=300s deployment/cert-manager-webhook  -n cert-manager
+
+#Install Ketch
+kubectl apply -f https://github.com/theketchio/ketch/releases/download/v0.7.0/ketch-controller.yaml
